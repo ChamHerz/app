83bc9196d408c77bf3513122280867098f7ce4b0
ChamHerz <adrielchambers@gmail.com>
2017-06-14 23:43:27 -0300

    modificaciones

--- src/main/java/MyResftfullApp/controller/VirtualMindApplication.java
+++ src/main/java/MyResftfullApp/controller/VirtualMindApplication.java
@@ -2,11 +2,16 @@ package MyResftfullApp.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
-@ComponentScan(basePackages="MyResftfullApp")
-public class VirtualMindApplication {
+//@SpringBootApplication
+//@ComponentScan(basePackages="MyResftfullApp")
+@SpringBootApplication(scanBasePackages = "MyResftfullApp.controller")
+@EntityScan(basePackages = "MyResftfullApp.controller")
+@EnableWebMvc
+public class VirtualMindApplication extends WebMvcAutoConfiguration {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VirtualMindApplication.class, args);
